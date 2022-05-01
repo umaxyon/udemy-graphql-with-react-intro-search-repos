3d51f51d3141ae2943a8d96dcfc710894fea568a
Umaxyon <umaxyon@gmail.com>
2022-05-02 01:02:53 +0900

    apply query and variable

--- src/graphql.js
+++ src/graphql.js
@@ -8,3 +8,30 @@ query me {
   }
 }
 `
+export const SEARCH_REPOSITORIES = gql`
+query searchRepo($first: Int, $after: String, $last: Int, $before: String, $query: String!) {
+    search(first: $first, after: $after, last: $last, before: $before, query: $query, type: REPOSITORY) {
+     repositoryCount
+     pageInfo {
+       endCursor
+       hasNextPage
+       hasPreviousPage
+       startCursor
+     }
+     edges {
+       cursor
+       node {
+         ... on Repository {
+           id
+             name
+           url
+           stargazers {
+             totalCount
+           }
+           viewerHasStarred
+         }
+       }
+     }
+    }  
+   }
+`
