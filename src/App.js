a9cbeada34b97d04cabdcf8429c23d1666dd8a65
Umaxyon <umaxyon@gmail.com>
2022-05-02 09:55:24 +0900

    titlize

--- src/App.js
+++ src/App.js
@@ -24,16 +24,25 @@ const Body = () => {
   }
 
   console.log({query})
-
+  if (!loading && !error) console.log(data.search)
+  
+  const render = () => {
+    const search = data.search;
+    const repositoryCount = search.repositoryCount;
+    const repositoryUnit = repositoryCount === 1 ? 'Repository' : 'Repositories'
+    const title = <h2>GitHub Repositories Search Results - {data.search.repositoryCount} {repositoryUnit}</h2>
+    return title
+  }
   return (
     <form>
       <input value={query} onChange={handleChange} />
     {
-      (loading) ? ( 'Loading...' 
+      (loading) ? (
+        'Loading...' 
       ) : (error) ? (
         `Error! ${error.message}`
       ) : (
-        <></>
+        render()
       )
     }
     </form>
